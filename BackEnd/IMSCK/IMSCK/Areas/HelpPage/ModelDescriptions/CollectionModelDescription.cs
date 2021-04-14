4545a14fc47172b33e1efbecb2fbf4f847baa152
DoruAlexandru <47774048+DoruAlexandru@users.noreply.github.com>
2021-04-14 18:31:56 +0300

    Delete BackEnd/IMSCK directory

--- BackEnd/IMSCK/IMSCK/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace IMSCK.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- BackEnd/IMSCK/IMSCK/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace IMSCK.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
